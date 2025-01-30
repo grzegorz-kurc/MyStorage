87019a17ed07ef0aae7415bbe30296a8f36e1fda
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-01-30 13:47:18 +0100

    Add UserRole enum and initial Category model

---
+++ MyStorageAPI/MyStorageAPI/Models/Data/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStorageAPI.Models.Data
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Color { get; set; } = "gray"; // TODO: retrieved from UI color picker in the future
+		public int WarehouseId { get; set; }
+		public Warehouse Warehouse { get; set; }
+		public ICollection<Product> Products { get; set; } = new List<Product>(); // Initialized with an empty list to avoid NullReferenceException when adding items. Ensures that the collection is never null
+	}
+}
---
+++ MyStorageAPI/MyStorageAPI/Models/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace MyStorageAPI.Models.Enums
+{
+    public enum UserRole
+    {
+        User = 1,
+        Admin = 2,
+        SuperAdmin = 3
+    }
+}
