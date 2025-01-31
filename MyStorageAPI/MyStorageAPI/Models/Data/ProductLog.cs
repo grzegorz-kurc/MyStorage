fe88528957d47e5cdaa2104f3f6684445ef50678
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-01-31 14:45:05 +0100

    Add ProductLog model

---
+++ MyStorageAPI/MyStorageAPI/Models/Data/ProductLog.cs
@@ -0,0 +1,14 @@
+﻿namespace MyStorageAPI.Models.Data
+{
+	public class ProductLog
+	{
+		public int Id { get; set; }
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
+		public string UserId { get; set; }
+		public User User { get; set; }
+		public int PreviousQuantity { get; set; }
+		public int NewQuantity { get; set; }
+		public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+	}
+}
