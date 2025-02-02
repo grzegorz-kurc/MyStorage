b034997ed72550fb81a64787242dcbbc8fe3b3e2
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-02-02 23:19:35 +0100

    Add UserAction enum

---
+++ MyStorageAPI/MyStorageAPI/Models/Enums/UserAction.cs
@@ -0,0 +1,15 @@
+﻿namespace MyStorageAPI.Models.Enums
+{
+	public enum UserAction
+	{
+		Login,
+		Logout,
+		FailedLogin,
+		PasswordReset,
+		AddProduct,
+		DeleteProduct,
+		AddUserToWarehouse,
+		RemoveUserFromWarehouse,
+		ChangeUserRole
+	}
+}
