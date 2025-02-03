06d8213b971f968c753cd853d0979fde7b1bb515
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-02-03 21:28:19 +0100

    Add UserLog

---
+++ MyStorageAPI/MyStorageAPI/Models/Data/UserLog.cs
@@ -0,0 +1,13 @@
+﻿using MyStorageAPI.Models.Enums;
+
+namespace MyStorageAPI.Models.Data
+{
+	public class UserLog
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; }
+		public User User { get; set; }
+		public UserAction Action { get; set; }
+		public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+	}
+}
