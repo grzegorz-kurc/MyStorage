5ace40665480fe2a2c3dd4645ca4344c4769029e
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-03-13 00:07:32 +0100

    Add RegisterResult model

---
+++ MyStorageAPI/MyStorageAPI/Models/Responses/RegisterResult.cs
@@ -0,0 +1,8 @@
+﻿namespace MyStorageAPI.Models.Responses
+{
+	public class RegisterResult
+	{
+		public bool Success { get; set; }
+		public List<string> Errors { get; set; } = new List<string>();
+	}
+}
