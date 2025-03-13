2ba040048a2868df9952a622efa1a64b67bdafe8
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-03-13 23:02:00 +0100

    Add AppConfig configuration model

---
+++ MyStorageAPI/MyStorageAPI/Models/Configuration/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace MyStorageAPI.Models.Configuration
+{
+	public class AppConfig
+	{
+		public EmailServiceConfig EmailService { get; set; } = new EmailServiceConfig();
+	}
+
+	public class EmailServiceConfig
+	{
+		public string ConsumerKey { get; set; } = string.Empty;
+		public string ConsumerSecret { get; set; } = string.Empty;
+		public string SenderEmail { get; set; } = string.Empty;
+		public string SenderName { get; set; } = string.Empty;
+	}
+}
