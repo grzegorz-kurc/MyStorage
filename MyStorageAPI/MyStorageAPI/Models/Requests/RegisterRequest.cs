4e8f1d15674c5c99b416284d63bccc6da9922d53
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-03-15 00:20:42 +0100

    Add validation rules for RegisterRequest model

--- MyStorageAPI/MyStorageAPI/Models/Requests/RegisterRequest.cs
+++ MyStorageAPI/MyStorageAPI/Models/Requests/RegisterRequest.cs
@@ -1,8 +1,15 @@
-﻿namespace MyStorageAPI.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStorageAPI.Models.Requests
 {
 	public class RegisterRequest
 	{
+		[Required(ErrorMessage = "Email is required.")]
+		[EmailAddress(ErrorMessage = "Invalid email format.")]
 		public string Email { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Password is required.")]
+		[MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
 		public string Password { get; set; } = string.Empty;
 	}
 }
