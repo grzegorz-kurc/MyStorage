ff419668b14884b54bab4b868b6d94796e27c526
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-03-24 23:04:35 +0100

    Add LoginResponse and LoginResult models

---
+++ MyStorageAPI/MyStorageAPI/Models/Responses/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MyStorageAPI.Models.Responses
+{
+	public class LoginResponse
+	{
+		public string Token { get; set; } = string.Empty;
+		public DateTime Expiration { get; set; }
+	}
+}
---
+++ MyStorageAPI/MyStorageAPI/Models/Responses/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace MyStorageAPI.Models.Responses
+{
+	public class LoginResult
+	{
+		public bool Success { get; set; }
+		public List<string> Errors { get; set; } = new();
+		public LoginResponse? Response { get; set; }
+	}
+}
