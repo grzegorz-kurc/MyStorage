46e554317099c493433725c90a9e28453efe84ef
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-04-02 23:33:38 +0200

    Add RefreshTokenRequest model

---
+++ MyStorageAPI/MyStorageAPI/Models/Requests/RefreshTokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MyStorageAPI.Models.Requests
+{
+	public class RefreshTokenRequest
+	{
+		public string Token { get; set; } = string.Empty; // expired access token
+		public string RefreshToken { get; set; } = string.Empty;
+	}
+}
