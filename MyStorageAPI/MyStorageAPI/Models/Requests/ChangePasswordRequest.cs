911fd3d6b3fcc6037a3290c393bd39327d276b67
Grzegorz Kurc <grzegorzkurc107@gmail.com>
2025-04-07 23:18:16 +0200

    Add ChangePasswordRequest model

---
+++ MyStorageAPI/MyStorageAPI/Models/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyStorageAPI.Models.Requests
+{
+	public class ChangePasswordRequest
+	{
+		public string CurrentPassword { get; set; } = string.Empty;
+		public string NewPassword { get; set; } = string.Empty;
+		public string ConfirmPassword { get; set; } = string.Empty;
+	}
+}
